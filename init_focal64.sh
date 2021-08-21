e9df369c9ee787e3c97be5b5f2a504627410bd3a
ian <ianlynxk@gmail.com>
2021-08-21 11:33:15 +0800

    添加 ubuntu 20.04 初始化脚本

---
+++ init_focal64.sh
@@ -0,0 +1,39 @@
+# !/bin/bash
+
+user=vagrant
+
+# 修改镜像源
+source_list="deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse"
+
+echo "$source_list" > /etc/apt/sources.list
+
+# 安装 docker
+apt -y update
+
+apt -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository -y\
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt -y update 
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+
+usermod -aG docker $user
